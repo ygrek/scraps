9e16ac0b4aef35611241aef5156eb08d36848f04
ygrek <ygrek@autistici.org>
2024-11-09 22:16:07 +0000

    detect wrong indentation after ocaml if

---
+++ indente.ml
@@ -0,0 +1,28 @@
+#! /usr/bin/env b0caml
+#directory "+ocaml/compiler-libs"
+
+open Printf
+
+let loc { Parsetree.pexp_loc = loc; _ } = Location.get_pos_info loc.loc_start
+let col expr = let (_,_,c) = loc expr in c
+
+let mapper = { Ast_mapper.default_mapper with
+  expr = fun mapper expr ->
+    begin match expr with
+    | { pexp_desc = Pexp_sequence ({ pexp_desc = Pexp_ifthenelse (_cond,e_then,e_else); _ } as e_if, next); _ } ->
+      let last = Option.value ~default:e_then e_else in
+      if col last = col next && col next <> col e_if then
+        let (file,line,_) = loc next in
+        printf "Suspicious indentation of next expression after if at %s %d\n%!" file line
+    | _ -> ()
+    end;
+    Ast_mapper.default_mapper.expr mapper expr
+  }
+
+let parse file =
+  ignore @@ mapper.structure mapper @@ Pparse.parse_implementation ~tool_name:"indetect" file
+
+let () =
+   Sys.argv |> Array.iteri begin fun i file ->
+     if i <> 0 then try parse file with exn -> fprintf stderr "Failed to parse %s : %s\n%!" file (Printexc.to_string exn)
+   end
